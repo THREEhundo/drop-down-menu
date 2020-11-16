e1b45c4cb10049856ad87aebae3639218133d3c7
THREEhundo <samuelbaik87@gmail.com>
2020-11-16 12:39:50 -0800

    function to create nav links & visibility on hovering in and out

--- js/main.js
+++ js/main.js
@@ -1,2 +1,37 @@
-//testing
-//123
+/*
+for loop -> li's
+append li's to ul
+event handlers for hover
+*/
+const ul = document.querySelector('#nav-list');
+const menu = ['Search', 'Home', 'JavaScript', 'CSS'];
+function createNav(num) {
+  for (let i = 0; i < num.length; i++) {
+    const li = document.createElement('li');
+    li.id = `nav-${i}`;
+    li.style.display = 'none';
+
+    const a = document.createElement('a');
+    a.id = `${num[i]}`;
+    a.href = '#';
+    a.innerHTML = `${num[i]}`;
+    ul.appendChild(li);
+    li.appendChild(a);
+  }
+}
+
+createNav(menu);
+
+ul.addEventListener('mouseover', () => {
+  const navItems = ul.children;
+  for (item of navItems) {
+    item.style.display = 'block';
+  }
+});
+
+ul.addEventListener('mouseout', () => {
+  const navItems = ul.children;
+  for (item of navItems) {
+    item.style.display = 'none';
+  }
+});
